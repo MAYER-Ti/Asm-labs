f900372034296bbe4be97e7737fe6302b01ca3c7
MAYER-Ti <s.lutov@inbox.ru>
2024-11-12 20:01:56 +0300

    write hello world

--- lab-3/hello_world.asm
+++ lab-3/hello_world.asm
@@ -1,16 +1,20 @@
-.model small
-.stack 100h
-.data 
-A DW 0,1,2,3,4,5,6,7,8,9
-B DW 10 DUP(0)
-
+.model	small
+.stack	100h
+.data
+Hello DB "Hello, world!", 0Dh, 0Ah,'$'
 .code
 start:
+; Для чтения A и B
+mov ax, @data
+mov ds, ax
+mov es, ax
+; записать указание на функцию DOS, которая выводит строку,
+; начинает с dx, заканчивает когда встречает $
+mov	ah,9
+lea     dx, Hello
+int     21h
 
-cld 
-lea      si,A
-lea      di,B
-mov      cx,10 
-rep      movsb
-
-end      start
+; Завершение программы
+mov ah, 4ch
+int 21h
+end	start
